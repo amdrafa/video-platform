d4c662ff8ef1f8d568356bf1b92a57637fa0aa94
unknown <tmrafinha4@gmail.com>
2023-05-21 19:13:05 -0300

    font changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    // Suas outras configurações aqui
-    output: {
-        // Configuração de exportação
-        export: {
-            // Ativar a exportação estática
-            static: true,
-        },
-    },
-};
+module.exports = nextConfig
